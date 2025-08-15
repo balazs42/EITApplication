16df89cd7d6c317ece3ce74b430d460190d5433e
balazs42 <65078193+balazs42@users.noreply.github.com>
2025-08-15 15:32:54 +0200

    Minor changes and renaming

--- ServiceLayer/IReconstructionService.cs
+++ ServiceLayer/IReconstructionService.cs
@@ -1,6 +1,7 @@
 ﻿using Utility.Classes;
 using Utility.Classes.Measurement;
 using Utility.Classes.Meshing.FiniteElementMesh;
+using Utility.Classes.Meshing.LatticeBoltzmannMesh;
 using Utility.Classes.ReconstructionParameters;
 
 namespace ServiceLayer
@@ -9,8 +10,15 @@ namespace ServiceLayer
     {
         public Task<ReconstructionResult> GetReconstructionResult();
         public void InitializeReconstruction(IMesh mesh, EITReconstructionParameters parameters);
-        public PotentialDistribution LBMSolveForward();
-        public ReconstructionResult LBMSolveInverse(int maxIterationCount);
+
+        // --- LBM Reconstruction ---
+
+        public PotentialDistribution SolveLbmForward();
+        public ReconstructionResult SolveLbmInverse(int maxIterationCount);
+        public EITMeasurement SimulateLbmMeasurements(LBMMesh mesh, double excitaionAmplitude);
+
+        // --- FEM Reconstruction
+
         public FEMMesh SolveFemForward(FEMMesh mesh);
         public FEMMesh SolveFemInverse(FEMMesh mesh, int maxIterCount, double stepSize, double regularization);
         public ReconstructionResult InverseSolveStepFem(FEMMesh mesh, double[] measurement, BoundaryCondition boundaryCondition, double stepSize);
