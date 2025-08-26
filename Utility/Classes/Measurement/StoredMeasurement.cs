a4c83818a193e94c8bbe6ae8a113c01f86df6ef5
balazs42 <65078193+balazs42@users.noreply.github.com>
2025-08-27 01:18:13 +0200

    Minor change, added stored measurement object

---
+++ Utility/Classes/Measurement/StoredMeasurement.cs
@@ -0,0 +1,11 @@
+﻿namespace Utility.Classes.Measurement
+{
+    public sealed class StoredMeasurement
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime SavedAt { get; set; }
+        public double[][] Frames { get; set; } = Array.Empty<double[]>();
+        public int FrameSize { get; set; }
+        public double? CurrentAmplitude { get; set; }
+    }
+}
