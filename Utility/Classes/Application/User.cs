50f4b91aaa2b3a26ac99c29d999132f3ae987cb6
balazs42 <65078193+balazs42@users.noreply.github.com>
2025-08-27 12:09:33 +0200

    Introduced Workspace to handle UI data and setups

---
+++ Utility/Classes/Application/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Utility.Classes.Application
+{
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Id { get; set; } = -1;
+
+        public User()
+        {
+            Name = "Test";
+            Email = "test@gmail.com";
+            Id = 0;
+        }
+
+        public User(string name, string email, int id)
+        {
+            Name = name;
+            Email = email;
+            Id = id;    
+        }
+    }
+}
