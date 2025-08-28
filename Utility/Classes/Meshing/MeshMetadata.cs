bc7ce2d60bb93aa642b2ddead28ddfab8923c418
balazs42 <65078193+balazs42@users.noreply.github.com>
2025-08-28 15:54:52 +0200

    feat: extend mesh serialization format

--- Utility/Classes/Meshing/MeshMetadata.cs
+++ Utility/Classes/Meshing/MeshMetadata.cs
@@ -8,5 +8,6 @@ namespace Utility.Classes.Meshing
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
         public string Generator { get; set; } = string.Empty;
         public Dictionary<string, string> Parameters { get; set; } = new();
+        public int ElementCount { get; set; }
     }
 }
