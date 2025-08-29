02d1fbbcd2cadad1e930048a329ad64eb6ce1565
balazs42 <65078193+balazs42@users.noreply.github.com>
2025-08-29 02:38:09 +0200

    Added user types

---
+++ Utility/Classes/Application/DefaultUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Utility.Classes.Application
+{
+    /// <summary>
+    /// Default User is the regular user, who can configure reconstruction, load measuremnets, and try several
+    /// reconstruction techniques. The default user does not have any further configuration access.
+    /// </summary>
+    public class DefaultUser : User
+    {
+    }
+}
--- Utility/Classes/Application/User.cs
+++ Utility/Classes/Application/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace Utility.Classes.Application
 {
-    public class User
+    public abstract class User
     {
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
