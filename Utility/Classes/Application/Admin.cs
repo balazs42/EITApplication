4807912bc4ce4dd5fcbe93c49f15c9ea7dbc6046
balazs42 <65078193+balazs42@users.noreply.github.com>
2025-08-29 02:38:09 +0200

    Added user types

---
+++ Utility/Classes/Application/Admin.cs
@@ -0,0 +1,11 @@
+﻿namespace Utility.Classes.Application
+{
+    /// <summary>
+    /// Admin is a type of user which has write access to overwrite DAQ data, access hardware logs
+    /// and modify several parameters, which is not available to a regular user.
+    /// </summary>
+    public class Admin : User
+    {
+
+    }
+}
