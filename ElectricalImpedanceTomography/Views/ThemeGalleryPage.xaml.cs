3cc1323a7604f0f72ea08063820b8d00e92dfce0
balazs42 <65078193+balazs42@users.noreply.github.com>
2025-09-29 21:22:26 +0200

    Fix AppThemeColor resource resolution

---
+++ ElectricalImpedanceTomography/Views/ThemeGalleryPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace ElectricalImpedanceTomography.Views;
+
+public partial class ThemeGalleryPage : ContentPage
+{
+    public ThemeGalleryPage()
+    {
+        InitializeComponent();
+    }
+}
